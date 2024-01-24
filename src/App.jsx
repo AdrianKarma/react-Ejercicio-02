003cda0b4716abadcb30c084e47f5859bad82be0
Adrian Chanampa <adrian.karma.chan@gmail.com>
2024-01-24 17:15:14 -0300

    utilizo props en el componente HelloFriend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import HelloFriend from './components/HelloFriend'
 
 function App() {
 
 
   return (
     <>
-      
+      <HelloFriend friend={"my Friend"} ></HelloFriend>
     </>
   )
 }
---
+++ src/components/HelloFriend.jsx
@@ -0,0 +1,11 @@
+
+
+const HelloFriend = (props) => {
+    return (
+        <div>
+            <h1>Hello {props.friend}!</h1>
+        </div>
+    );
+};
+
+export default HelloFriend;
